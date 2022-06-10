936e825944c88716deeb6ea74f739a449a135210
JamesDev5 <jamesmendoncajm@gmail.com>
2022-06-10 09:05:56 -0300

    Criação do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+<Header/>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import Header from "@/components/layouts/Header.vue"
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Header
   },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body{
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap');
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 </style>
---
+++ src/components/layouts/Header.vue
@@ -0,0 +1,55 @@
+<template>
+<nav class="container-topo">
+    <h1 class="logo">Codar.</h1>
+  <ul class="menu-navigation">
+    <li><a href="#home">Inicio</a></li>
+    <li><a href="#services">Serviços</a></li>
+    <li><a href="#Contact">Contatos</a></li>
+    <button class="btn-login">Login</button>
+  </ul>
+</nav>
+</template>
+
+<script>
+export default {
+      name:"Header"
+}
+</script>
+
+<style scoped>
+.container-topo{
+  margin: 20px ;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.menu-navigation{
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+
+  gap: 20px;
+  font-weight: 700;
+}
+.logo{
+  color:#D6000D;
+}
+li{
+  list-style: none;
+  
+}
+a{
+  text-decoration: none;
+  color: black;
+}
+.btn-login{
+  font-weight: 700;
+  width: 100px;
+  height: 30px;
+  background-color: #D6000D;
+  color: white;
+  border: none;
+  border-radius: 10px;
+}
+
+</style>
